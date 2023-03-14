930c6625cd8fa080d62ba427da981f63ffec4d00
sting7401 <sting7401@naver.com>
2023-03-14 17:34:01 +0900

    r

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/chunks/internal.js
+++ .vercel/output/functions/fn.func/.svelte-kit/output/server/chunks/internal.js
@@ -155,7 +155,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "16otxhx"
+  version_hash: "286bel"
 };
 function get_hooks() {
   return {};
--- svelte.config.js
+++ svelte.config.js
@@ -18,7 +18,10 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+
+		adapter: adapter({
+			runtime: 'nodejs18.x',
+		}),
 		alias: {
 			// these are the aliases and paths to them
 			$components: path.resolve('./src/lib/components'),
